20fac0f2f66d71483e21701f35bff745aa0f879b
dynamic2006 <tejasvi.aynor@gmail.com>
2026-05-05 21:49:40 -0700

    stress testing with 10 crossing knot

--- khovanov.py
+++ khovanov.py
@@ -283,7 +283,8 @@ class KhovanovComplex:
             print(f"rank H_{degree} = {self.get_free_rank(degree)}")
     
 
-K = KhovanovComplex("bCdA")
+# K = KhovanovComplex("bCdA")
+# K = KhovanovComplex("bfjihgaedc") # stress test
 
 # for state in range(1 << K.n):
 #     for crossing in range(K.n):
